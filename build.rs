cc4bf1f2ba47adf69e54a772256734b15a954f1e
rogeryoungh <rogeryoungh@foxmail.com>
2024-11-24 16:28:55 +0800

    feat: default require admin

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+	if cfg!(target_os = "windows") {
+			let mut res = winres::WindowsResource::new();
+			res
+			.set_manifest_file("./assets/app.manifest");
+			res.compile().expect("Failed to compile resources");
+	}
+}
